6ff6b54552c07bb1a94805d1dc0e01ac8cb47f84
Nirvana Lal <93890030+nirvanalal@users.noreply.github.com>
2021-11-17 00:31:21 +0000

    test

---
+++ chris/uwi/p1/test.py
@@ -0,0 +1,16 @@
+# just chris here.... NOPE
+x = 5
+
+def set_x(num):
+    # Local var x not the same as global variable x
+    x = num    # => 43
+    print(x)   # => 43
+
+def set_global_x(num):
+    global x
+    print(x)   # => 5
+    x = num    # global var x is now set to 6
+    print(x)   # => 6
+
+set_x(43)
+set_global_x(6)

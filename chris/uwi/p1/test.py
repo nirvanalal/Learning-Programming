039ccba1353dc2cc13d6b5a96a4a489a949b110a
Nirvana Lal <93890030+nirvanalal@users.noreply.github.com>
2021-11-18 04:04:41 +0000

    test

--- chris/uwi/p1/test.py
+++ chris/uwi/p1/test.py
@@ -12,5 +12,19 @@ def set_global_x(num):
     x = num    # global var x is now set to 6
     print(x)   # => 6
 
-set_x(43)
-set_global_x(6)
+# set_x(43)
+# set_global_x(6)
+
+# Python has first class functions
+def create_adder(x):
+    def adder(y):
+        return x + y
+    return adder
+
+add_10 = create_adder(10)
+add_10(3)   # => 13
+
+# print (adder)
+
+import math
+dir(math)
